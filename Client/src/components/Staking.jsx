818063f2fd78491f14b5fe64dbf5b03589b11953
Elevatedpeace <Peace08182443958>
2022-03-23 15:47:13 +0100

    cool

--- Client/src/components/Staking.jsx
+++ Client/src/components/Staking.jsx
@@ -6,8 +6,8 @@ const style = {
 
 const Staking = () => {
   return (
-    <div className="bg-red-500 h-96 ">
-      <div className=" w-64 h-96 bg-gray-900 space-y-3 px-4">
+    <div className="blue-glassmorphism  h-[45rem] ">
+      <div className=" w-55 h-86   space-y-3 px-14">
         <Header />
         <Main />
       </div>
@@ -17,7 +17,7 @@ const Staking = () => {
 
 const Header = () => {
   return (
-    <div className={style.ava}>
+    <div className={style.ava} >
       <p>1 KTk = 4000$ </p>
     </div>
   );
@@ -25,7 +25,7 @@ const Header = () => {
 
 const Main = () => {
   return (
-    <div className=" space-y-3">
+    <div className=" blue-glassmorphism space-y-3">
       <Circle />
       <AvailableStaked />
       <Payment />
@@ -36,14 +36,16 @@ const Main = () => {
 
 const Circle =()=> {
   return (
-    <div className=" rounded-full w-52 h-52 bg-white flex justify-center items-center">
-      <div className="w-44 h-44 rounded-full bg-blue-500 flex justify-center items-center">
-        <div className="w-36 h-36 rounded-full bg-blue-900 flex flex-col space-y-1 justify-center items-center">
-          <span className="text-sm text-gray-300  ">Total Balance</span>
-          <p className="text-bold text-white font-bold text-2xl">89.004</p>
-          <p className="text-bold text-white text-sm uppercase self-center font-bold">
-            KTK
-          </p>
+    <div className="flex space-x-6 justify-center items-center">
+      <div className=" rounded-full w-52 h-52 bg-white flex justify-center m-16 items-center ">
+        <div className="w-44 h-44 rounded-full bg-blue-500 flex justify-center items-center">
+          <div className="w-36 h-36 rounded-full bg-blue-900 flex flex-col space-y-1 justify-center items-center">
+            <span className="text-sm text-gray-300  ">Total Balance</span>
+            <p className="text-bold text-white font-bold text-2xl">89.004</p>
+            <p className="text-bold text-white text-sm uppercase self-center font-bold">
+              KTK
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -77,23 +79,34 @@ const Payment = () => {
  })
 };
   return (
-    <div className=" flex space-x-4">
+    <div className="  flex-col flex space-y-5 justify-center items-center  ">
       <div
         onClick={() => pop()}
-        className=" px-4 py-1 bg-blue-400 rounded-lg text-gray-100 font-semibold cursor-pointer"
+        className="  w-[18rem] px-[6.5rem] m-4 py-[0.5rem]  bg-blue-500 rounded-lg text-gray-100    font-semibold cursor-pointer"
       >
         Buy Token
       </div>
 
       <div
         onClick={() => pop()}
-        className=" w-24 px-[1.8rem] cursor-pointer py-1 ring-1 rounded-lg text-gray-100 font-semibold"
+        className=" w-[18rem] px-[7.7rem] m-4 cursor-pointer py-[0.4rem] p-14 ring-1  rounded-lg text-gray-100  font-semibold"
       >
         Stake
+      </div>
+      <div
+        onClick={() => pop()}
+        className=" w-[18rem] px-[7.7rem] m-4 cursor-pointer py-[0.4rem] p-14  bg-yellow-600 rounded-lg text-gray-100  font-semibold"
+      >
+        claim
+      </div>
+      <div
+        onClick={() => pop()}
+        className=" w-[18rem] px-[7.3rem] m-3 cursor-pointer py-[0.4rem]   bg-red-600 rounded-lg text-gray-100  font-semibold"
+      >
+        unstake
       </div>
     </div>
   );
 };
-
 
 export default Staking;
